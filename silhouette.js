4562de69564986bdda76a29d62131fa0da268f4a
Nagisa Sano <terunori56100@gmail.com>
2025-12-16 14:56:22 +0100

    Silhouette Engine v1.0 - Structure HTML/CSS + design Ghesquière

---
+++ silhouette.js
@@ -0,0 +1,9 @@
+function generate() {
+    const rules = {
+        shoulder: ['sharp', 'volume', 'fluid'],
+        col: ['asym', 'vneck', 'oversize']
+    };
+    // 20 combinaisons via nested loops
+    // Canvas: dessine formes SVG (lignes courbes, rectangles déformés)
+    // Output: 10 canvases + fiches "Tension: Haute | Fluidité: Moyenne"
+}
